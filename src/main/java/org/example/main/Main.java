515372d18f6b88cfd983c5c94896fd5fad51f9a7
Thavasumoorthi-S <thavasumoorthi.s@tringapps.com>
2023-03-02 21:48:42 +0530

    first commit

--- src/main/java/org/example/main/Main.java
+++ src/main/java/org/example/main/Main.java
@@ -22,11 +22,66 @@ public class Main {
     public static void main(String[] args) throws IOException{
         int choice;
         Scanner sc=new Scanner(System.in);
-            do {
+            while(true) {
                 LOGGER.info("\n)Bank\n2)shape\n3)student\n4)singleton\n5)coordinates\n6)credit card\n7)Exception\n8)Studentdata\n9)Array,Arraylist,Linkedlist\n10)hashset,treeset,hashmap\n11)calculator\n12)studentcontacts using Linkedlist\n13)file operation \n14)Tictactoe game\n15)Exit");
                 LOGGER.info("Choose any one of the option in the above ");
                 choice = sc.nextInt();
-                if (choice == 1) {
+                switch(choice)
+                {
+                    case 1:
+                        Bank.execute();
+
+                    case 2:
+                        Shape.execute();
+
+                    case 3:
+                        StudentTest.execute();
+
+                    case 4:
+                        Singleton.execute();
+
+                    case 5:
+                        Coordinates.execute();
+
+                    case 6:
+                        Creditcard.execute();
+
+
+                    case 7:
+                        Exception.execute();
+                    case 8:
+                        Students.execute();
+
+                    case 9:
+                        LinkedList.execute();
+
+                    case 10:
+                        Hashset.excute();
+
+                    case 11:
+                        Calculator1.execute();
+
+                    case 12:
+                        Contact.execute();
+
+
+                    case 13:
+                        Fileopds.execute();
+
+                    case 14:
+                        Games.execute();
+
+                    default:
+                        LOGGER.info("please enter valid input");
+                        break;
+                }
+            }
+    }
+}
+
+
+/*
+if (choice == 1) {
                     Bank.execute();
                 }
                 else if (choice == 2) {
@@ -68,7 +123,56 @@ public class Main {
                 else{
                     LOGGER.info("please choose correct choice in the above");
                 }
-        }while (true);
-    }
-}
+ */
 
+
+/*
+switch(case)
+    {
+        case 1:
+            Bank.execute();
+
+        case 2:
+            Shape.execute();
+
+        case 3:
+            StudentTest.execute();
+
+        case 4:
+            Singleton.execute();
+
+        case 5:
+            Coordinates.execute();
+
+        case 6:
+            Creditcard.execute();
+
+
+        case 7:
+            Exception.execute();
+        case 8:
+            Students.execute();
+
+        case 9:
+            LinkedList.execute();
+
+        case 10:
+            Hashset.excute();
+
+        case 11:
+            Calculator1.execute();
+
+        case 12:
+            Contact.execute();
+
+
+        case 13:
+            Fileopds.execute();
+
+        case 14:
+            Games.execute();
+
+        default:
+            LOGGER.info("please enter valid input");
+    }
+ */
